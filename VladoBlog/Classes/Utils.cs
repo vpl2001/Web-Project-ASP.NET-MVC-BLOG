6b19a0492ed7c0bdb68d39f88328be5b02f4919f
vpl2001 <vladislav.lozanov@abv.bg>
2016-08-24 14:26:40 +0300

    Blog System
    Soft Uni Course Software Technologies 2016

---
+++ VladoBlog/Classes/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VladoBlog.Classes
+{
+    public class Utils
+    {
+        public static string CutText(string text, int maxLenght = 150)
+        {
+            if (text==null || text.Length<=maxLenght)
+            {
+                return text;
+            }
+            else
+            {
+                var shortText = text.Substring(0, maxLenght)+".....";
+                return shortText;
+            }
+        }
+    }
+}
---
+++ VladoBlog/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VladoBlog.Models;
+using System.Data.Entity;
+
+namespace VladoBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            var post = db.Posts.Include(p => p.Author).OrderByDescending(p => p.Date).Take(3);
+            return View(post.ToList());
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
